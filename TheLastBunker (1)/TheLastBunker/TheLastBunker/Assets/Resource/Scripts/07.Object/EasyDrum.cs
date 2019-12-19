32820d4266ae3d823af17435fa67da9c1294a875
jung-yul-ho <wjddufgh@gmail.com>
2019-12-19 14:49:25 +0900

    jungyulho
    빌드파일 업로드

--- TheLastBunker (1)/TheLastBunker/TheLastBunker/Assets/Resource/Scripts/07.Object/EasyDrum.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class EasyDrum : Object
-{
-    public Sprite BrokenSprite;
-    public GameObject InvisibleObj;
-}
